42a7325518ea73689b8c2fecc5780e1a5987e65b
Alex <alex.java.it@gmail.com>
2017-10-06 10:10:20 -0400

    ADDING UPDATE remove repeating characters in a string

--- Remove repeating characters in a string/src/Remove.java
+++ Remove repeating characters in a string/src/Remove.java
@@ -13,14 +13,18 @@ public class Remove {
 	public static String removeCharacters(String str){
 	    String ourString="";
 	    int j = 0;
-	    for (int i=0; i<str.length() ; i++){
-	        j = i+1;
-	        if(i==str.length()-1){
-	        	if(str.charAt(i-1)!=str.charAt(i-2))
-	        		ourString+=str.charAt(i);
-			} else if (str.charAt(i)!=str.charAt(j)) {
-	        	ourString+=str.charAt(i);
-			}
+	    if(str.isEmpty()){
+	    	return null;
+	    }else{
+	    	ourString+= str.charAt(0);
+		    for (int i=0; i<str.length()-1 ; i++){
+		        j = i+1;
+		     
+		        
+				 if (str.charAt(i)!=str.charAt(j)) {
+		        	ourString+=str.charAt(j);
+				}
+		    }
 	    }
 	    return ourString;
 	}
